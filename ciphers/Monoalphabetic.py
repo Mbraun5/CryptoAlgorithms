4a893109a69f3da48c230d0e828ce94c8252e325
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-14 05:05:54 -0700

    added Monoalphabetic cipher pseudocode

---
+++ ciphers/Monoalphabetic.py
@@ -0,0 +1,34 @@
+from .base import CipherInterface, CipherException
+
+
+class Monoalphabetic(CipherInterface):
+    def __init__(self, key="", ifil="", ofil="", mode=""):
+        super().__init__(key=key, ifil=ifil, ofil=ofil)
+        self.encrypt() if mode=="ENC" else self.decrypt()
+        self.write_output()
+
+    def setKey(self, key):
+        """
+        Sets the key to use for the cipher
+
+        :param str key: The key to use
+        :return: True if key is valid, False otherwise
+        """
+        pass
+
+    def encrypt(self):
+        """
+        Encrypt the plaintext.
+        """
+        pass
+
+    def decrypt(self):
+        """
+        Decrypt the ciphertext.
+        """
+        pass
+
+    def key_exception(self, key):
+        # Add custom exception.
+        # raise CipherException("")
+        pass
