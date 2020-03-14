265433773bc7ac80467b92bc76016263026ae9fb
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-13 23:25:35 -0700

    restructured cipher into class

--- cipher.py
+++ cipher.py
@@ -1,56 +1,48 @@
 from ciphers import *
 import argparse
 
-def set_arguments():
-    pass
 
 class Main():
-    def __init__(self):
-        self.set_arguments()
+    @classmethod
+    def get_args(cls):
+        cls.parser = argparse.ArgumentParser(
+            description="Encrypt plaintext using various ciphers. Provides functionality for Playfair, \
+                Row Transposition, Railfence, Vigenre, and Monoalphabetic Cipher."
+        )
+        cls.parser.add_argument("<CIPHER NAME>", help="Name of cipher to use. One of `PLF`, `RTS`, `RFC`, `VIG`, `CES`, `MAC`.")
+        cls.parser.add_argument("<KEY>", help="The encryption/decryption key to use.")
+        cls.parser.add_argument("<ENC/DEC>", help="One of `ENC` or `DEC`: whether to encrypt or decrypt, respectively.")
+        cls.parser.add_argument("<INPUT FILE>", help="The file from which to read the input.")
+        cls.parser.add_argument("<OUTPUT FILE>", help="The file to which the output should be written.")
+        return vars(cls.parser.parse_args())
+    
+    @classmethod
+    def process_args(cls):
+        args = cls.get_args()
+        cipher_name = args["<CIPHER NAME>"]
+        cipher_arguments = {
+            "key": args["<KEY>"],
+        }
+        ciphers = {
+            "PLF": Playfair, 
+            "RTS": 1, 
+            "RFC": 2, 
+            "VIG": 3, 
+            "CES": 4, 
+            "MAC": 5
+        }
+        try:
+            return ciphers[cipher_name](**cipher_arguments)
+        except KeyError:
+            cls.parser.error("Invalid cipher name. <CIPHER NAME> must be one of `PLF`, `RTS`, `RFC`, `VIG`, `CES`, `MAC`.")
 
-    def set_arguments(self):
-        pass
+    @classmethod
+    def main(cls):
+        cipher = cls.process_args()
+        # cipher.encrypt()
+        # cipher.decrypt()
+
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Encrypt plaintext using various ciphers. Provides functionality\
-                                        for Playfair, Row Transposition, Railfence, Vigenre, and Monoalphabetic Cipher.")
-    parser.add_argument(
-        "<CIPHER NAME>", 
-        help="Name of the cipher to use. One of `PLF`, `RTS`, `RFC`, `VIG`, `CES`, `MAC`.",
-    )
-    parser.add_argument(
-        "<KEY>", 
-        help="The encryption/decryption key to use.",
-    )
-    parser.add_argument(
-        "<ENC/DEC>", 
-        help="One of `ENC` or `DEC`: whether to encrypt or decrypt, respectively.",
-    )
-    parser.add_argument(
-        "<INPUT FILE>", 
-        help="The file from which to read the input.",
-    )
-    parser.add_argument(
-        "<OUTPUT FILE>", 
-        help="The file to which the output should be written.",
-    )
-    args = vars(parser.parse_args())
-    
-    cipher_name = args["<CIPHER NAME>"]
-    cipher_arguments = {
-        "key": args["<KEY>"],
-    }
-    ciphers = {
-        "PLF": Playfair(**cipher_arguments), 
-        "RTS": 1, 
-        "RFC": 2, 
-        "VIG": 3, 
-        "CES": 4, 
-        "MAC": 5
-    }
-
-    try:
-        cipher = ciphers[cipher_name]
-    except KeyError:
-        parser.error("Invalid cipher name. <CIPHER NAME> must be one of `PLF`, `RTS`, `RFC`, `VIG`, `CES`, `MAC`.")
+    Main.main()
