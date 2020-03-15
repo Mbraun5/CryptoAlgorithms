591860c185c7f5ce6270dc27eee7c0852b057347
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-14 19:54:38 -0700

    added integration tests for travis

--- tests/Integration.py
+++ tests/Integration.py
@@ -1,8 +1,25 @@
 import unittest
 import os
 from cipher import Main
+import filecmp
 
 
 class TestAlgorithms(unittest.TestCase):
     def test_playfair(self):
-        pass
+        for file in os.listdir("./Playfair"):
+            with self.subtest(file=file):
+                if file.startswith("dec_"):
+                    pass
+                elif file.startswith("enc_"):
+                    pass
+                else:
+                    Main.test_integration("PLF", file[:3], "./Playfair/{}".format(file), "test_enc.txt", "ENC")
+                    Main.test_integration("PLF", file[:3], "./Playfair/{}".format(file), "test_dec.txt", "DEC")
+                self.assertTrue(filecmp.cmp("./Playfair/enc_{}".format(file), "test_enc.txt", shallow=False))
+                self.assertTrue(filecmp.cmp("./Playfair/dec_{}".format(file), "test_dec.txt", shallow=False))
+                os.remove("test_enc.txt")
+                os.remove("test_dec.txt")
+
+
+if __name__ == "__main__":
+    unittest.main()
