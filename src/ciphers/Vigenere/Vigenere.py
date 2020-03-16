549552652ee7d1b17ec2555d155f85071279caf9
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-15 20:40:11 -0700

    added initial code for Vigenere Cipher

--- src/ciphers/Vigenere/Vigenere.py
+++ src/ciphers/Vigenere/Vigenere.py
@@ -14,13 +14,23 @@ class Vigenere(CipherInterface):
         :param str key: The key to use
         :return: True if key is valid, False otherwise
         """
-        pass
+        if not key.isalpha():
+            return False
+        while len(key) < len(self.itxt):
+            key += key
+        self.key = key[:len(self.itxt)].upper()
+        return True
 
     def encrypt(self):
         """
         Encrypt the plaintext.
         """
-        pass
+        self.otxt = ""
+        for idx, char in enumerate(self.itxt):
+            if ord(char) + ord(self.key[idx]) - ord('A') > ord('Z'):
+                self.otxt += chr(ord(self.key[idx]) - (ord('Z') - ord(char)) - 1)
+            else:
+                self.otxt += chr(ord(char) + ord(self.key[idx]) - ord('A'))
 
     def decrypt(self):
         """
@@ -29,6 +39,4 @@ class Vigenere(CipherInterface):
         pass
 
     def key_exception(self, key):
-        # Add custom exception.
-        # raise CipherException("")
-        pass
+        raise CipherException("Invalid key: {}. Vigenere cipher key must be a string only containing characters".format(key))
