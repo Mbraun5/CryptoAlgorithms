ae2dbfb08fbade197291f86407bf7728129a66ef
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-15 19:23:49 -0700

    added Railfence Cipher code

--- src/ciphers/Railfence/Railfence.py
+++ src/ciphers/Railfence/Railfence.py
@@ -1,4 +1,5 @@
 from CipherInterface.CipherInterface import CipherInterface, CipherException
+import math
 
 
 class Railfence(CipherInterface):
@@ -14,21 +15,40 @@ class Railfence(CipherInterface):
         :param str key: The key to use
         :return: True if key is valid, False otherwise
         """
-        pass
+        if not key.isdigit():
+            return False
+        self.key = int(key)
+        return True
 
     def encrypt(self):
         """
         Encrypt the plaintext.
         """
-        pass
+        ctl = [[] for _ in range(self.key)]
+        for idx, char in enumerate(self.itxt):
+            ctl[idx % self.key].append(char)
+        self.otxt = "".join(["".join(li) for li in ctl])
 
     def decrypt(self):
         """
         Decrypt the ciphertext.
         """
-        pass
+        nchr = math.floor(len(self.itxt) / self.key)
+        xtr = len(self.itxt) % self.key
+        self.otxt = ""
+        for idx in range(nchr):
+            cnt = xtr
+            for row in range(self.key):
+                if row == 0:
+                    self.otxt += self.itxt[idx]
+                elif cnt > 0:
+                    self.otxt += self.itxt[idx + (nchr + 1) * row]
+                    cnt -= 1
+                else:
+                    self.otxt += self.itxt[idx + (nchr + 1) * xtr + nchr * (row - xtr)]
+        if xtr > 0:
+            for idx in range(xtr):
+                self.otxt += self.itxt[(nchr + 1) * (idx + 1) - 1]
 
     def key_exception(self, key):
-        # Add custom exception.
-        # raise CipherException("")
-        pass
+        raise CipherException("Invalid key: {}. Railfence cipher key must be a single integer such as `12`".format(key))
