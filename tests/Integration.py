3c184c27b6ff705480b66a48cbbbf1ad2ab486e9
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-16 18:47:08 -0700

    added integration test for monoalphabetic cipher

--- tests/Integration.py
+++ tests/Integration.py
@@ -87,6 +87,22 @@ class TestAlgorithms(unittest.TestCase):
                     os.remove("test_enc.txt")
                     os.remove("test_dec.txt")
 
+    def test_monoalphabetic(self):
+        testdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "Monoalphabetic")
+        for file in os.listdir(testdir):
+            with self.subTest(file=file):
+                if file.startswith("dec_"):
+                    continue
+                elif file.startswith("enc_"):
+                    continue
+                else:
+                    Main.test_integration("MAC", file[:len(file)-4], "{}/{}".format(testdir, file), "test_enc.txt", "ENC")
+                    Main.test_integration("MAC", file[:len(file)-4], "test_enc.txt", "test_dec.txt", "DEC")
+                    self.assertTrue(filecmp.cmp("{}/enc_{}".format(testdir, file), "test_enc.txt", shallow=False))
+                    self.assertTrue(filecmp.cmp("{}/dec_{}".format(testdir, file), "test_dec.txt", shallow=False))
+                    os.remove("test_enc.txt")
+                    os.remove("test_dec.txt")
+
 
 if __name__ == "__main__":
     unittest.main()
