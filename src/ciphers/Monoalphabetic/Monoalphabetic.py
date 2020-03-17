237d94a30542a1fea70e2b814ce902d25e8018b3
Matthew Braun <braun.matthewwork@gmail.com>
2020-03-16 18:46:16 -0700

    added code for monoalphabetic cipher

--- src/ciphers/Monoalphabetic/Monoalphabetic.py
+++ src/ciphers/Monoalphabetic/Monoalphabetic.py
@@ -14,21 +14,35 @@ class Monoalphabetic(CipherInterface):
         :param str key: The key to use
         :return: True if key is valid, False otherwise
         """
-        pass
+        if not key.isalpha():
+            return False
+        self.key = key.upper()
+        sum = 0
+        self.encdi = {}
+        self.decdi = {}
+        for index, char in enumerate(self.key):
+            sum += (ord(char) - ord('A') + 1)
+            self.encdi[chr(index + ord('A'))] = char
+            self.decdi[char] = chr(index + ord('A'))
+        return sum == (26 * 27) / 2
 
     def encrypt(self):
         """
         Encrypt the plaintext.
         """
-        pass
+        self.otxt = ""
+        for char in self.itxt:
+            self.otxt += self.encdi[char]
 
     def decrypt(self):
         """
         Decrypt the ciphertext.
         """
-        pass
+        self.otxt = ""
+        for char in self.itxt:
+            self.otxt += self.decdi[char]
 
     def key_exception(self, key):
-        # Add custom exception.
-        # raise CipherException("")
-        pass
+        raise CipherException("Invalid key: {}. For the Monoalphabetic Cipher, the key must contain only alphabetic characters".format(key)\
+                            + " and each character in the alphabet must be used exactly once. Letters in the alphabet are mapped to the key"\
+                            + " based on position in the string. That is, a->index_0, b->index_1, ..., z->index_25.")
